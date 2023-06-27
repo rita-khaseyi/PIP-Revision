adf1603f32af62b5863d3ca1487a95dc66768589
Nathalieelizabeth <elizabethsnathalie@gmail.com>
2023-06-27 09:07:59 +0300

    added question two

--- pip.kt
+++ pip.kt
@@ -4,6 +4,41 @@ fun main() {
     var translating = Translator("Khosa","Alberto","English")
     println(translating.translate())
     println(telling.tellStory())
+
+
+    val jollofRice = NigerianRecipe(
+        "Jollof Rice",
+        "Nigeria",
+        listOf("rice", "tomatoes", "onions", "peppers", "chicken"),
+        "1 hour",
+        "Steamed",
+        listOf("fats", "carbs", "proteins"),
+        true
+    )
+    println(jollofRice.nigerianJollofRice())
+    val injera = EthiopianRecipe(
+        "Injera",
+        "Ethiopia",
+        listOf("dough", "onions", "tomatoes", "apricots", "almonds"),
+        "2 hours",
+        "fried",
+        listOf("fat", "carbs", "vitamins"),
+        "so soft"
+    )
+    println(injera.injeraMeal())
+    val moroccanChicken = MoroccanRecipe(
+        "Moroccan Chicken with Tangerine",
+        "Morocco",
+        listOf(
+            "chicken", "tangerine", "onion", "garlic", "olive oil",
+            "cumin", "coriander", "cinnamon", "paprika", "salt", "pepper"
+        ),
+        "1 hour",
+        "grilling",
+        listOf("high in protein and vitamin C"),
+        true
+    )
+    println(moroccanChicken.moroccanChickenTangerine())
 }
     open class Story(
         var length: String,
@@ -38,4 +73,61 @@ fun main() {
         }
     }
 
-
+//2.
+    open class Recipe(
+    val name: String,
+    val country: String,
+    val ingredients: List<String>,
+    val preparationTime: String,
+    val cookingMethod: String,
+    val nutritionInfo: List<String>
+)
+class MoroccanRecipe(
+    name: String,
+    country: String,
+    ingredients: List<String>,
+    preparationTime: String,
+    cookingMethod: String,
+    nutritionInfo: List<String>,
+    val spicy: Boolean
+) : Recipe(name, country, ingredients, preparationTime, cookingMethod, nutritionInfo) {
+    fun moroccanChickenTangerine(): String {
+        return if (ingredients.contains(cookingMethod)) {
+            "The Moroccan food is prepared with $ingredients using $cookingMethod."
+        } else {
+            "That is not Moroccan food."
+        }
+    }
+}
+class NigerianRecipe(
+    name: String,
+    country: String,
+    ingredients: List<String>,
+    preparationTime: String,
+    cookingMethod: String,
+    nutritionInfo: List<String>,
+    val pepperish: Boolean
+) : Recipe(name, country, ingredients, preparationTime, cookingMethod, nutritionInfo) {
+    fun nigerianJollofRice(): String {
+        return if (preparationTime.isNotBlank() && nutritionInfo.contains(pepperish.toString())) {
+            "The Nigerian food is prepared with $ingredients for $preparationTime. " +
+                    "It has to be $cookingMethod for it to have $nutritionInfo and must have $pepperish."
+        } else {
+            "That is not a well-prepared Jollof Rice."
+        }
+    }
+}
+class EthiopianRecipe(
+    name: String,
+    country: String,
+    ingredients: List<String>,
+    preparationTime: String,
+    cookingMethod: String,
+    nutritionInfo: List<String>,
+    val soft: String
+) : Recipe(name, country, ingredients, preparationTime, cookingMethod, nutritionInfo) {
+    fun injeraMeal(): String {
+        return "Injera is cooked using $ingredients for $preparationTime. " +
+                "It is sweet when $cookingMethod for it to have $nutritionInfo and must be $soft."
+    }
+}
